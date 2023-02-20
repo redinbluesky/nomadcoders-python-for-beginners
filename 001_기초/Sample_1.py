417a7456e0e78af51c9f81dcb17febfb54d7e2c4
ParkJuYeonbg <redinbluesky@gmail.com>
2023-02-20 16:45:29 +0900

    Request

---
+++ 001_기초/Sample_1.py
@@ -0,0 +1,20 @@
+from requests import get
+
+websites = (
+    "google.com",
+    "airbnb.com",
+    "https://www.python.org/"
+)
+
+results = {}
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}" 
+    response = get(website)
+    if(response.status_code == 200):
+       results[website] = "OK"
+    else:
+       results[website] = "FAILED"
+       
+print(results)
--- 001_기초/data.py
+++ 001_기초/data.py
@@ -1,8 +1,31 @@
-#days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
-#print(days_of_week)
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+print(days_of_week)
+days_of_week.append(["🍔🍕"])
+print(days_of_week[-1])
 
 # tuple 생성 후 변경 할 수 없다.
 days = ("Mon","Tue","Wed","Thur","Fri")
 print(days[-5])
+
+# Dictionary
+player = {
+    'name': 'nico',
+    'age': 12,
+    'alive': True,
+    'fav_food': ["🍕","🍔"]
+}
+
+print(player.get('age'))
+print(player.get('fav_food'))
+print(player['fav_food'])
+
+player.pop('age')
+print(player)
+
+player['xp'] = 1500
+print(player)
+
+player['fav_food'].append("🍿")
+print(player)
 
 
