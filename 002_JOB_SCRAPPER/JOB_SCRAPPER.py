cf345a121e589a15166727a1c8e783710f7db031
ParkJuYeonbg <redinbluesky@gmail.com>
2023-02-21 15:06:59 +0900

    JOB_SCRAPPER Done

--- 002_JOB_SCRAPPER/JOB_SCRAPPER.py
+++ 002_JOB_SCRAPPER/JOB_SCRAPPER.py
@@ -1,35 +1,15 @@
-from requests import get
-from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_job
 
-base_url = "https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term=";
-search_teram="python"
+keyword = input("What do you want to search for?")
 
-response = get(f"{base_url}{search_teram}")
-if response.status_code != 200:
-    print("Can't request website")
-else:
-    results = []
-    soup = BeautifulSoup(response.text, 'html.parser')
-    jobs = soup.find_all('section',class_='jobs')
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1); # 마지막 버튼 li 제거
-        for post in job_posts:
-            anchors = post.find_all("a")
-            anchor = anchors[1]
-            link = anchor['href']
-            company, kind, region = anchor.find_all('span',class_="company")
-            title = anchor.find('span',class_='title')
-            job_data = {
-                'link':f"https://weworkremotely.com{link}",
-                'company': company.string,
-                'region': region.string,
-                'position': title.string
-            }
-            results.append(job_data)
-    for result in results:
-        print(result)
-        print("//////////////////")
-    
-    
-    
+file = open(f"{keyword}.csv", "w",encoding='utf-8' )
+
+file.write("Position,Company,Location,URL\n")
+
+jobs = extract_wwr_job("python")
+
+for job in jobs:
+    print(f"{job['position']},{job['company']},{job['region']},{job['link']}")
+    file.write(f"{job['position']},{job['company']},{job['region']},{job['link']}\n")
+
+file.close()
